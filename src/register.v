63228256ab2c3b1030871421ba988c0f585a61a7
charlespers <charlespers.m@gmail.com>
2026-02-07 16:43:40 -0500

    Finished v1 of the reg file implementation documentation included

--- src/register.v
+++ src/register.v
@@ -10,4 +10,32 @@ module regfile #(
     input wire [$clog2(REGS)-1:0] rd,
     input wire [$clog2(REGS)-1:0] rs1,
     input wire [$clog2(REGS)-1:0] rs2,
-)
+    output wire [THREADS*DATA_W-1:0] rs1_data, // bus
+    output wire [THREADS*DATA_W-1:0] rs2_data, 
+    input wire [THREADS*DATA_W-1:0] rd_data
+);
+    /store
+    reg [DATA_W-1:0] regfile [0:THREADS-1] [0:REGS-1];
+    integer t;
+
+    // read
+    generate
+        genvar i;
+        for (i=0l i < THREADS; i = i+1) begin : READ_PORTS
+            assign rs1_data[i*DATA_W +: DATA_W] = regfile[i][rs1];
+            assign rs2_data[i*DATA_W +: DATA_W] = regfile[i][rs2];
+        end
+    endgenerate
+
+    // write
+    always @(posedge clk) begin
+        if (we) begin
+            for (t=0; t <THREADS; t = t+1) begin
+                if (active_mask[t]) begin
+                    regfile[t][rd] <= rd_data[t*DATA_W +: DATA_W];
+                end
+            end
+        end
+
+    end
+endmodule
